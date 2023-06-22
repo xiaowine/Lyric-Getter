0645d8b91d5ae708508bf012ba9121772d211063
xiaowine <xiaowine0@gmail.com>
2023-06-22 19:21:34 +0800

    酷我单独启用定时判断是否运行

--- app/src/main/java/cn/lyric/getter/hook/app/Kuwo.kt
+++ app/src/main/java/cn/lyric/getter/hook/app/Kuwo.kt
@@ -1,7 +1,10 @@
 package cn.lyric.getter.hook.app
 
 
+import android.content.Context
+import android.media.AudioManager
 import cn.lyric.getter.hook.BaseHook
+import cn.lyric.getter.tool.EventTools.cleanLyric
 import cn.lyric.getter.tool.EventTools.sendLyric
 import cn.lyric.getter.tool.HookTools
 import cn.lyric.getter.tool.HookTools.context
@@ -14,6 +17,8 @@ import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import io.luckypray.dexkit.DexKitBridge
 import io.luckypray.dexkit.enums.MatchType
+import java.util.Timer
+import java.util.TimerTask
 
 
 object Kuwo : BaseHook() {
@@ -24,10 +29,34 @@ object Kuwo : BaseHook() {
 
     override val name: String get() = this.javaClass.simpleName
 
+    val audioManager by lazy { context.getSystemService(Context.AUDIO_SERVICE) as AudioManager }
+
+    private var timer: Timer? = null
+    private var isRunning = false
+    private fun startTimer() {
+        timer = Timer()
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                if (!audioManager.isMusicActive) {
+                    cleanLyric(context)
+                    stopTimer()
+                }
+            }
+        }, 0, 1000)
+        isRunning = true
+    }
+
+    private fun stopTimer() {
+        timer?.cancel()
+        isRunning = false
+    }
+
+
     override fun init() {
         loadClassOrNull("cn.kuwo.mod.playcontrol.RemoteControlLyricMgr").isNotNull {
             it.methodFinder().first { name == "updateLyricText" }.createHook {
                 after { param ->
+                    if (!isRunning) startTimer()
                     sendLyric(context, param.args[0].toString(), context.packageName)
                 }
             }
@@ -43,6 +72,7 @@ object Kuwo : BaseHook() {
                         if (!res.declaringClassName.contains("ui") && res.isMethod) {
                             loadClass(res.declaringClassName).methodFinder().first { name == res.name }.createHook {
                                 after { hookParam ->
+                                    if (!isRunning) startTimer()
                                     sendLyric(context, hookParam.args[0].toString(), context.packageName)
                                 }
                             }
