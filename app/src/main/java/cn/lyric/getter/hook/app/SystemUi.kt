cfef6ab4d7254cdbab3d81fd6ef67b9ba4c848a1
xiaowine <xiaowine0@gmail.com>
2023-06-23 15:57:07 +0800

    添加判断，防止部分机型没有class导致报错

--- app/src/main/java/cn/lyric/getter/hook/app/SystemUi.kt
+++ app/src/main/java/cn/lyric/getter/hook/app/SystemUi.kt
@@ -6,7 +6,9 @@ import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.EventTools
 import cn.lyric.getter.tool.EventTools.TAG
 import cn.lyric.getter.tool.HookTools.context
+import cn.lyric.getter.tool.Tools.isNotNull
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.github.kyuubiran.ezxhelper.finders.ConstructorFinder.`-Static`.constructorFinder
@@ -17,25 +19,27 @@ object SystemUi : BaseHook() {
     private var lastStats: Boolean = false
     override val name: String get() = this.javaClass.simpleName
     override fun init() {
-        loadClass("com.android.systemui.media.MediaCarouselController").methodFinder().first { name == "removePlayer" }.createHook {
-            after {
-                Log.d(TAG, "removePlayer")
-                EventTools.cleanLyric(context)
+        loadClassOrNull("com.android.systemui.media.MediaCarouselController").isNotNull {
+            it.methodFinder().first { name == "removePlayer" }.createHook {
+                after {
+                    EventTools.cleanLyric(context)
+                }
             }
         }
-        loadClass("com.android.systemui.media.MediaData").constructorFinder().first().createHook {
-            after {
-                it.thisObject.objectHelper {
-                    val stats = getObjectOrNullAs<Boolean>("isPlaying") ?: false
-                    Log.d(TAG, "isPlaying: $stats")
-                    if (lastStats != stats) {
-                        lastStats = stats
-                        if (!stats) {
-                            EventTools.cleanLyric(context)
+        loadClassOrNull("com.android.systemui.media.MediaData").isNotNull {
+            it.constructorFinder().first().createHook {
+                after { hookParam ->
+                    hookParam.thisObject.objectHelper {
+                        val stats = getObjectOrNullAs<Boolean>("isPlaying") ?: false
+                        if (lastStats != stats) {
+                            lastStats = stats
+                            if (!stats) {
+                                EventTools.cleanLyric(context)
+                            }
                         }
                     }
-                }
 
+                }
             }
         }
 //        "com.android.systemui.media.MediaData".findClass().hookAfterAllConstructors {
