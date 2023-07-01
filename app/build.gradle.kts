0d1a90aa21cbb31e8fad7a483c49600a82a15321
xiaowine <xiaowine0@gmail.com>
2023-07-01 18:14:26 +0800

    适配部分系统

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cn.lyric.getter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1.1"
 
         buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
     }
--- app/src/main/java/cn/lyric/getter/hook/app/SystemUi.kt
+++ app/src/main/java/cn/lyric/getter/hook/app/SystemUi.kt
@@ -4,6 +4,7 @@ package cn.lyric.getter.hook.app
 import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.EventTools
 import cn.lyric.getter.tool.HookTools.context
+import cn.lyric.getter.tool.Tools.isNot
 import cn.lyric.getter.tool.Tools.isNotNull
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
@@ -15,29 +16,46 @@ object SystemUi : BaseHook() {
 
     private var lastStats: Boolean = false
     override val name: String get() = this.javaClass.simpleName
-    override fun init() {
-        loadClassOrNull("com.android.systemui.media.MediaCarouselController").isNotNull {
-            it.methodFinder().first { name == "removePlayer" }.createHook {
-                after {
-                    EventTools.cleanLyric(context)
-                }
+    private fun removePlayer(it: Class<*>) {
+        it.methodFinder().first { name == "removePlayer" }.createHook {
+            after {
+                EventTools.cleanLyric(context)
             }
         }
-        loadClassOrNull("com.android.systemui.media.MediaData").isNotNull {
-            it.constructorFinder().first().createHook {
-                after { hookParam ->
-                    hookParam.thisObject.objectHelper {
-                        val stats = getObjectOrNullAs<Boolean>("isPlaying") ?: false
-                        if (lastStats != stats) {
-                            lastStats = stats
-                            if (!stats) {
-                                EventTools.cleanLyric(context)
-                            }
+    }
+
+    private fun isPlaying(it: Class<*>) {
+        it.constructorFinder().first().createHook {
+            after { hookParam ->
+                hookParam.thisObject.objectHelper {
+                    val stats = getObjectOrNullAs<Boolean>("isPlaying") ?: false
+                    if (lastStats != stats) {
+                        lastStats = stats
+                        if (!stats) {
+                            EventTools.cleanLyric(context)
                         }
                     }
-
                 }
             }
         }
+    }
+
+    override fun init() {
+        loadClassOrNull("com.android.systemui.media.MediaCarouselController").isNotNull {
+            removePlayer(it)
+        }.isNot {
+            loadClassOrNull("com.android.systemui.media.controls.ui.MediaCarouselController").isNotNull {
+                removePlayer(it)
+            }
+        }
+
+        loadClassOrNull("com.android.systemui.media.MediaData").isNotNull {
+            isPlaying(it)
+        }.isNot {
+            loadClassOrNull("com.android.systemui.media.controls.models.player.MediaData").isNotNull {
+                isPlaying(it)
+            }
+        }
+
     }
 }
