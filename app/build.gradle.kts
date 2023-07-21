67c4215c8f88c4624ba7998d77bf128bbf6d2827
xiao_wine <xiaowine0@gmail.com>
2023-07-21 23:20:13 +0800

    1.0.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "cn.lyric.getter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.4.$apiVersion"
+        versionCode = 7
+        versionName = "1.0.5.$apiVersion"
 
         buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
         buildConfigField("int", "API_VERSION", "$apiVersion")
