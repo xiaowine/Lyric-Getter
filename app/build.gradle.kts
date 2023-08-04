4b9383200da01c5bd6f4ed83e6857755b5593923
xiao_wine <xiaowine0@gmail.com>
2023-08-04 21:02:33 +0800

    2.0.0 API to 5 添加UI 添加 允许部分软件息屏后依旧输出 添加 允许输出重复歌词 修复 Apple music异常

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "cn.lyric.getter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.5$buildTime"
+        versionCode = 8
+        versionName = "2.0.0"
         buildConfigField("long", "BUILD_TIME", "$buildTime")
         buildConfigField("int", "API_VERSION", "5")
         buildConfigField("int", "CONFIG_VERSION", "1")
