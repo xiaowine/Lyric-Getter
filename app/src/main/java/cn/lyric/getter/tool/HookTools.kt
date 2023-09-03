1810ab1e415cfbbb7bb95305eca2575c9d766854
xiao_wine <68009112+xiaowine@users.noreply.github.com>
2023-09-04 00:56:47 +0800

    Revert "Revert "[Bugfix]拦截QQLite的占位歌词 (#34)" (#35)" (#36)
    This reverts commit 96306c25cc32ae21c3e583023d167a36f8a10fd0.

--- app/src/main/java/cn/lyric/getter/tool/HookTools.kt
+++ app/src/main/java/cn/lyric/getter/tool/HookTools.kt
@@ -179,6 +179,7 @@ object HookTools {
             remoteControlManager.methodFinder().first { name == "updataMetaData" }.createHook {
                 before {
                     val lyric = if (it.args[1].isNull()) return@before else it.args[1].toString()
+                    if ("NEED_NOT_UPDATE_TITLE" == lyric) return@before
                     sendLyric(context, lyric)
                 }
             }
