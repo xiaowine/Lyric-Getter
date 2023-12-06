155009b4d25074d133d58a66525aa4778959ab2f
xiao_wine <xiaowine0@gmail.com>
2023-12-06 20:24:10 +0800

    agp to 8.2.0 修复网易云音乐极速版部分问题

--- app/src/main/java/cn/lyric/getter/hook/app/NeteaseLite.kt
+++ app/src/main/java/cn/lyric/getter/hook/app/NeteaseLite.kt
@@ -1,45 +1,38 @@
 package cn.lyric.getter.hook.app
 
-import android.view.View
+import android.widget.LinearLayout
 import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.HookTools
-import cn.xiaowine.xkt.LogTool.log
 import cn.xiaowine.xkt.Tool
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
-import com.github.kyuubiran.ezxhelper.finders.ConstructorFinder.`-Static`.constructorFinder
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
-import org.luckypray.dexkit.DexKitBridge
 
 object NeteaseLite : BaseHook() {
     init {
         System.loadLibrary("dexkit")
     }
 
-    var lyric: String by Tool.observableChange("") { _, _, newValue ->
-        HookTools.eventTools.sendLyric(newValue)
+    private var nowLyric: String = ""
+
+    private var nextLyric: String by Tool.observableChange("") { _, _, newValue ->
+        if (nowLyric.isNotEmpty()) {
+            HookTools.eventTools.sendLyric(nowLyric)
+        }
+        nowLyric = newValue
     }
 
     override fun init() {
         super.init()
         loadClass("com.netease.cloudmusic.meta.LyricLine").methodFinder().filterByName("getContent").first().createHook {
             after {
-                lyric = it.result.toString()
+                nextLyric = it.result.toString()
             }
         }
-        HookTools.getApplication { application ->
-            DexKitBridge.create(application.classLoader, false).use { dexKitBridge ->
-                dexKitBridge.apply {
-                    val single = findClass {
-                        matcher {
-                            addEqString("JwsARRhTXw==")
-                        }
-                    }.log()!!.single()
-                    loadClass(single.name).constructorFinder().first().createHook {
-                        after {
-                            (it.thisObject as View).visibility = View.GONE
-                        }
-                    }
+        loadClass("android.view.WindowManagerImpl").methodFinder().first { name == "addView" }.createHook {
+            after { view ->
+                if (view.args[0]::class.java.name.contains("floatlyric")) {
+                    (view.args[0] as LinearLayout).removeAllViews()
                 }
             }
         }
