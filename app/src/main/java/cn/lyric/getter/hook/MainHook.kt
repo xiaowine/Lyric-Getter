efffbe7f4be944eb8ad9b1d02b6ba0aceaf0b679
xiao_wine <xiaowine0@gmail.com>
2024-05-14 14:35:05 +0800

    适配 拟声

--- app/src/main/java/cn/lyric/getter/hook/MainHook.kt
+++ app/src/main/java/cn/lyric/getter/hook/MainHook.kt
@@ -12,6 +12,7 @@ import cn.lyric.getter.hook.app.LMusic
 import cn.lyric.getter.hook.app.Luna
 import cn.lyric.getter.hook.app.Meizu
 import cn.lyric.getter.hook.app.MiPlayer
+import cn.lyric.getter.hook.app.Mimicry
 import cn.lyric.getter.hook.app.MobileMusic
 import cn.lyric.getter.hook.app.MusicFree
 import cn.lyric.getter.hook.app.Netease
@@ -56,6 +57,7 @@ class MainHook : IXposedHookLoadPackage, IXposedHookZygoteInit {
             "com.lalilu.lmusic" -> initHooks(LMusic)
             "cn.wenyu.bodian" -> initHooks(Bodian)
             "fun.upup.musicfree" -> initHooks(MusicFree)
+            "com.mimicry.mymusic" -> initHooks(Mimicry)
             else -> initHooks(Api)
         }
     }
---
+++ app/src/main/java/cn/lyric/getter/hook/app/Mimicry.kt
@@ -0,0 +1,11 @@
+package cn.lyric.getter.hook.app
+
+import cn.lyric.getter.hook.BaseHook
+import cn.lyric.getter.tool.HookTools
+
+object Mimicry:BaseHook() {
+    override fun init() {
+//        super.init()
+        HookTools.MockFlyme().notificationLyric()
+    }
+}
