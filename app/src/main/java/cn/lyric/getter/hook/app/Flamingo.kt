e38b33a17dd0d8d782baa24aad5173ec1fa1fe75
Memory <134070804+Memory2314@users.noreply.github.com>
2024-06-10 22:18:05 +0800

    添加Api判断

--- app/src/main/java/cn/lyric/getter/hook/app/Flamingo.kt
+++ app/src/main/java/cn/lyric/getter/hook/app/Flamingo.kt
@@ -2,11 +2,14 @@ package cn.lyric.getter.hook.app
 
 
 import cn.lyric.getter.hook.BaseHook
+import cn.lyric.getter.tool.HookTools.isApi
 
 object Flamingo : BaseHook() {
 
     override fun init() {
         super.init()
-        Api.hook()
+        if isApi {
+            Api.hook()
+        }
     }
 }
