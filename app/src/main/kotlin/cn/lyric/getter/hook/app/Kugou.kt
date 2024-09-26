a2d4a960b3861231a7852bbaee54cc429c0547af
李太白 <36808766+ghhccghk@users.noreply.github.com>
2024-09-26 19:04:48 +0800

    酷狗更换成状态栏hook,同时保留车载hook (#192)
    * 分离酷狗和酷狗概念版hook，并更换成状态栏hook,同时保留车载hook
    
    测试过程发现有些版本会因为腾讯Tinker会导致崩溃
    
    Signed-off-by: ghhccghk <2137610394@qq.com>
    
    * 优化逻辑，撤销分离，并修改fuckTinker来保证不会崩溃（效果应该有的）
    
    Signed-off-by: ghhccghk <2137610394@qq.com>
    
    * Update Kugou.kt
    
    * 修复无法正常在高版本安卓获取版本号
    
    Signed-off-by: ghhccghk <2137610394@qq.com>
    
    ---------
    
    Signed-off-by: ghhccghk <2137610394@qq.com>
    Co-authored-by: 余空 <70465933+YuKongA@users.noreply.github.com>

--- app/src/main/kotlin/cn/lyric/getter/hook/app/Kugou.kt
+++ app/src/main/kotlin/cn/lyric/getter/hook/app/Kugou.kt
@@ -1,25 +1,83 @@
 package cn.lyric.getter.hook.app
 
-import cn.lyric.getter.tool.EventTools
+import android.content.Intent
 import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.HookTools
-import cn.lyric.getter.tool.HookTools.context
 import cn.lyric.getter.tool.HookTools.eventTools
+import cn.lyric.getter.tool.HookTools.fuckTinker
+import cn.lyric.getter.tool.Tools.getVersionCode
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.EzXHelper.classLoader
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 
 object Kugou : BaseHook() {
-
     override fun init() {
         super.init()
+        fuckTinker()
         HookTools.openBluetoothA2dpOn()
-        HookTools.fuckTinker()
-        loadClass("com.kugou.framework.player.c").methodFinder().filterByParamTypes(HashMap::class.java).first { name == "a" }.createHook {
-            after {
-                val hashMap = it.args[0] as HashMap<*, *>
-                eventTools.sendLyric(hashMap[0].toString())
+        HookTools.getApplication { app ->
+            val verCode: Int = app.packageManager?.getPackageInfo(app.packageName, 0)?.getVersionCode() ?: 0
+            when (app.packageName) {
+                "com.kugou.android" -> {
+                    when {
+                        verCode <= 10000 -> hookMethodForVersionA()
+                        verCode <= 12009 -> {
+                            HookTools.MockFlyme().mock()
+                            hookLocalBroadcast("android.support.v4.content.LocalBroadcastManager")
+                        }
+
+                        verCode <= 999999 -> {
+                            HookTools.MockFlyme().mock()
+                            hookLocalBroadcast("androidx.localbroadcastmanager.content.LocalBroadcastManager")
+                        }
+                    }
+                }
+
+                "com.kugou.android.lite" -> {
+                    when {
+                        verCode <= 10648 -> hookMethodForVersionA()
+                        verCode <= 10999 -> {
+                            HookTools.MockFlyme().mock()
+                            hookLocalBroadcast("android.support.v4.content.LocalBroadcastManager")
+                        }
+
+                        verCode > 11001 -> {
+                            HookTools.MockFlyme().mock()
+                            hookLocalBroadcast("androidx.localbroadcastmanager.content.LocalBroadcastManager")
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private fun hookMethodForVersionA() {
+        loadClass("com.kugou.framework.player.c").methodFinder()
+            .filterByParamTypes(HashMap::class.java).first { name == "a" }
+            .createHook {
+                after {
+                    val hashMap = it.args[0] as HashMap<*, *>
+                    eventTools.sendLyric(hashMap[0].toString())
+                }
+            }
+    }
+
+    private fun hookLocalBroadcast(className: String) {
+        loadClass(className, classLoader).methodFinder()
+            .first { name == "sendBroadcast" }
+            .createHook {
+                before {
+                    val intent = it.args[0] as Intent
+                    val action = intent.action
+                    val message = intent.getStringExtra("lyric")
+                    //action.log()
+                    if (action == "com.kugou.android.update_meizu_lyric") {
+                        message?.let { it3 ->
+                            eventTools.sendLyric(it3)
+                        }
+                    }
+                }
+            }
     }
 }
