13cf485e3574c2dffeffd0f4e9a00ac316cf72bb
xiao_wine <xiaowine0@gmail.com>
2024-10-06 22:49:35 +0800

    修复酷狗音乐会重复获取歌词

--- app/src/main/kotlin/cn/lyric/getter/hook/app/Kugou.kt
+++ app/src/main/kotlin/cn/lyric/getter/hook/app/Kugou.kt
@@ -1,15 +1,19 @@
 package cn.lyric.getter.hook.app
 
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
 import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.HookTools
 import cn.lyric.getter.tool.HookTools.eventTools
 import cn.lyric.getter.tool.HookTools.fuckTinker
 import cn.lyric.getter.tool.Tools.getVersionCode
+import cn.xiaowine.xkt.LogTool.log
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.EzXHelper.classLoader
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+import android.os.Process
 
 object Kugou : BaseHook() {
     override fun init() {
@@ -20,6 +24,7 @@ object Kugou : BaseHook() {
             val verCode: Int = app.packageManager?.getPackageInfo(app.packageName, 0)?.getVersionCode() ?: 0
             when (app.packageName) {
                 "com.kugou.android" -> {
+                    if (getProcessName(app) == "com.kugou.android") return@getApplication
                     when {
                         verCode <= 10000 -> hookMethodForVersionA()
                         verCode <= 12009 -> {
@@ -50,6 +55,17 @@ object Kugou : BaseHook() {
                 }
             }
         }
+    }
+
+    private fun getProcessName(context: Context): String? {
+        val pid = Process.myPid()
+        val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (process in manager.runningAppProcesses) {
+            if (process.pid == pid) {
+                return process.processName
+            }
+        }
+        return null
     }
 
     private fun hookMethodForVersionA() {
