63ae4236b2d9c6a9abe57fade02cfadb709a891c
重量级 <38679706+xxx-source@users.noreply.github.com>
2024-11-24 16:27:39 +0800

    添加 “音乐播放器” 支持 (#245)
    * Update app_rules.json
    
    增加对 “音乐播放器” 规则
    
    * Update arrays.xml
    
    “音乐播放器” 支持
    
    * 添加对 “音乐播放器” 的 hook 文件
    
    * Update MainHook.kt
    
    添加 “音乐播放器” 支持

---
+++ app/src/main/kotlin/cn/lyric/getter/hook/app/MusicPlayer.kt
@@ -0,0 +1,15 @@
+package cn.lyric.getter.hook.app
+
+
+import cn.lyric.getter.hook.BaseHook
+import cn.lyric.getter.tool.HookTools.isApi
+
+object MusicPlayer : BaseHook() {
+
+    override fun init() {
+        super.init()
+        isApi {
+            Api.hook()
+        }
+    }
+}
