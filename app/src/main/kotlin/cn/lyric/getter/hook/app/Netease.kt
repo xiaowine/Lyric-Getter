fdbba01fcc1a328b87e398f7148c176e494b8a08
李太白 <36808766+ghhccghk@users.noreply.github.com>
2024-12-01 19:21:52 +0800

    修复网易云音乐hook修改机型导致澎湃系统爱心按钮和歌词按钮异常 (#249)
    * 修改歌词hook点
    
    * 修复网易云音乐hook修改机型导致澎湃系统爱心按钮和歌词按钮异常

--- app/src/main/kotlin/cn/lyric/getter/hook/app/Netease.kt
+++ app/src/main/kotlin/cn/lyric/getter/hook/app/Netease.kt
@@ -1,6 +1,7 @@
 package cn.lyric.getter.hook.app
 
 import android.annotation.SuppressLint
+import android.os.Build
 import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.HookTools
 import cn.lyric.getter.tool.HookTools.MockFlyme
@@ -8,11 +9,24 @@ import cn.lyric.getter.tool.HookTools.dexKitBridge
 import cn.lyric.getter.tool.HookTools.eventTools
 import cn.lyric.getter.tool.HookTools.fuckTinker
 import cn.lyric.getter.tool.HookTools.mediaMetadataCompatLyric
+import cn.lyric.getter.tool.MeiZuNotification
 import cn.lyric.getter.tool.Tools.getVersionCode
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+import java.lang.reflect.Method
 
+private fun Method.replaceName() {
+    this.createHook {
+        after {
+            when (it.args[0].toString()) {
+                "FLAG_ALWAYS_SHOW_TICKER" -> it.result = MeiZuNotification::class.java.getDeclaredField("FLAG_ALWAYS_SHOW_TICKER_HOOK")
+                "FLAG_ONLY_UPDATE_TICKER" -> it.result = MeiZuNotification::class.java.getDeclaredField("FLAG_ONLY_UPDATE_TICKER_HOOK")
+            }
+        }
+    }
+}
 
 @SuppressLint("StaticFieldLeak")
 object Netease : BaseHook() {
@@ -22,7 +36,14 @@ object Netease : BaseHook() {
 
     override fun init() {
         super.init()
-        MockFlyme().mock()
+        loadClass("android.os.SystemProperties").methodFinder().first { name == "get" }.createHook {
+            after {
+                setStaticObject(Build::class.java, "DISPLAY", "Flyme")
+
+            }
+        }
+        Class::class.java.methodFinder().first { name == "getField" }.replaceName()
+        Class::class.java.methodFinder().first { name == "getDeclaredField" }.replaceName()
         fuckTinker()
         HookTools.getApplication {
             val verCode = it.packageManager?.getPackageInfo(it.packageName, 0)?.getVersionCode() ?: 0
