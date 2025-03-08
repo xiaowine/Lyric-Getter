04e635fa908db2db17626e090e6a34976312016c
ghhccghk <2137610394@qq.com>
2025-03-08 10:30:52 +0800

    fix：优化酷狗概念版，OPPO音乐歌词获取

--- app/src/main/kotlin/cn/lyric/getter/hook/app/Oppo.kt
+++ app/src/main/kotlin/cn/lyric/getter/hook/app/Oppo.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import cn.lyric.getter.hook.BaseHook
 import cn.lyric.getter.tool.HookTools
 import cn.lyric.getter.tool.HookTools.dexKitBridge
+import cn.lyric.getter.tool.Tools.getVersionCode
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
@@ -14,6 +15,8 @@ object Oppo : BaseHook() {
         System.loadLibrary("dexkit")
     }
     override fun init() {
+
+        /** 绕过oppo设备限制 */
         loadClass("android.os.SystemProperties").methodFinder().first { name == "get" }.createHook {
             after {
                 setStaticObject(Build::class.java, "BRAND", "oppo")
@@ -23,84 +26,26 @@ object Oppo : BaseHook() {
             }
         }
         HookTools.mediaMetadataCompatLyric()
-        HookTools.getApplication {
-            dexKitBridge(it.classLoader) { dexKitBridge ->
-                val sboppo = dexKitBridge.findClass {
-                    matcher {
-                        usingStrings("非蓝牙连接成功自动播放的场景不需要拦截")
+        HookTools.getApplication { app ->
+            /** 版本号获取预留 不同软件包名分离方便未来调整hook规则*/
+            val verCode: Int = app.packageManager?.getPackageInfo(app.packageName, 0)?.getVersionCode() ?: 0
+            dexKitBridge(app.classLoader) { dexKitBridge ->
+                when (app.packageName) {
+                    "com.heytap.music" -> {
+                        /** 强开蓝牙歌词 */
+                        loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "l" }.createHook {
+                            after{ params ->
+                                params.result = true
+                            }
+                        }
                     }
-                }.single()
-                loadClass("com.allsaints.localpush.LocalPushReceiver").methodFinder().first{ name == "goto" }.createHook {
-                    after{ params ->
-                        //" 测试1 ${params.result }".log()
-                        //" 测试1传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass(sboppo.name).methodFinder().first{ name == "b" }.createHook {
-                    after{ params ->
-                        //" 测试2 ${params.result }".log()
-                        //" 测试2传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "do" }.createHook {
-                    after{ params ->
-                        //" 测试3 ${params.result }".log()
-                        //" 测试3传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "G" }.createHook {
-                    after{ params ->
-                        //" 测试4 ${params.result} }".log()
-                        //" 测试4传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                    before {
-                        val field = it.thisObject.javaClass.getDeclaredField("b")
-                        field.isAccessible = true // 确保可以访问私有字段
-                        //field.setBoolean(it.thisObject, true) // 设置值为 true
-                        val fieldc = it.thisObject.javaClass.getDeclaredField("const")
-                        fieldc.isAccessible = true // 确保可以访问私有字段
-                        //fieldc.setBoolean(it.thisObject, true) // 设置值为 true
-                        val fieldd = it.thisObject.javaClass.getDeclaredField("catch")
-                        fieldd.isAccessible = true // 确保可以访问私有字段
-                        //fieldd.setBoolean(it.thisObject, true) // 设置值为 true
-                    }
-                }
-                loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "F" }.createHook {
-                    after{ params ->
-                        //" 测试5 ${params.result}}".log()
-                        //" 测试5传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "class" }.createHook {
-                    after{ params ->
-                        //" 测试6 ${{params.result}}".log()
-                        //" 测试6传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "C" }.createHook {
-                    after{ params ->
-                        //" 测试7 ${params.result}".log()
-                        //" 测试7传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass("com.allsaints.music.player.PlayManager").methodFinder().first{ name == "z4" }.createHook {
-                    after{ params ->
-                        //" 测试8 ${params.result}".log()
-                        //" 测试8传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                loadClass("com.allsaints.music.player.PlayManager").methodFinder().first{ name == "y4" }.createHook {
-                    after{ params ->
-                        //" 测试9 ${params.result}".log()
-                        //" 测试9传入 ${Arrays.toString(params.args)  }".log()
-                    }
-                }
-                /** 强开蓝牙歌词 */
-                loadClass(sboppo.name).methodFinder().first{ name == "l" }.createHook {
-                    after{ params ->
-                        //" 测试10 ${params.result}".log()
-                        params.result = true
-                       // " 测试10传入 ${Arrays.toString(params.args)  }".log()
+                    "com.oppo.music" -> {
+                        /** 强开蓝牙歌词 */
+                        loadClass("com.allsaints.music.player.thirdpart.MediaSessionHelper").methodFinder().first{ name == "l" }.createHook {
+                            after{ params ->
+                                params.result = true
+                            }
+                        }
                     }
                 }
             }
