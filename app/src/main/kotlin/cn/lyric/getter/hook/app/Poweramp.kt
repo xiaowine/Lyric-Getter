b03678956ef13a0b251968b504f903d05c899a90
ghhccghk <2137610394@qq.com>
2025-03-20 12:17:46 +0800

    fix:优化poweramp兼容

--- app/src/main/kotlin/cn/lyric/getter/hook/app/Poweramp.kt
+++ app/src/main/kotlin/cn/lyric/getter/hook/app/Poweramp.kt
@@ -1,34 +1,40 @@
 package cn.lyric.getter.hook.app
 
-import android.database.CharArrayBuffer
-import android.view.ViewGroup
 import cn.lyric.getter.hook.BaseHook
-import cn.lyric.getter.tool.HookTools
-import cn.lyric.getter.tool.HookTools.dexKitBridge
 import cn.lyric.getter.tool.HookTools.eventTools
 import cn.lyric.getter.tool.HookTools.extractValues
-import cn.lyric.getter.tool.JsonTools.toJSON
-import cn.xiaowine.xkt.LogTool.log
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
-import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
-import de.robv.android.xposed.XposedBridge
-import java.util.Arrays
-
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder
 
 object Poweramp : BaseHook() {
 
     override fun init() {
         super.init()
-        loadClass("com.maxmpz.widget.player.list.LyricsFastTextView").methodFinder().first { name == "l0"}.createHook {
+        val clazz = loadClass("com.maxmpz.widget.player.list.LyricsFastTextView")
+        val method = MethodFinder.fromClass(clazz)
+            .filter {
+                val params = parameterTypes
+                params.size >= 4 && params[1] == Boolean::class.java && params[2] == Int::class.java && params[3] == Int::class.java
+            }
+            .single()
+        method.createHook {
             before {
                 //"返回 ${it.result}".log()
                 //"l0传入 ${Arrays.toString(it.args)}".log()
                 val xc = it.args[0] // `XC` 参数，包含歌词信息
+                val c = it.args[2] //判断歌词是否为现在的
                 val a = xc.toString()
                 val b = extractValues(a,"text")
                 if (!b.isNullOrEmpty()) {
-                    eventTools.sendLyric(b)
+                    if (b != "null" ){
+                        if (c != 0){
+                            eventTools.sendLyric(b)
+                        }
+                    } else {
+                        eventTools.cleanLyric()
+                    }
+
                 }
             }
         }
