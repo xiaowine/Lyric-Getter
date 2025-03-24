95d3b6b96219fcb28c7547dca4e9045479b4daa0
ghhccghk <2137610394@qq.com>
2025-03-24 18:45:30 +0800

    fix:修复dialog里输入框不会上移

---
+++ app/src/main/kotlin/cn/lyric/getter/ui/activity/DialogTransparentActivity.kt
@@ -0,0 +1,36 @@
+package cn.lyric.getter.ui.activity
+
+import android.content.Context
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import cn.lyric.getter.R
+import cn.lyric.getter.tool.ConfigTools.config
+import cn.lyric.getter.ui.dialog.EditTextDialog
+
+
+class DialogTransparentActivity: AppCompatActivity() {
+
+    private fun openRegexReplaceDialog(context: Context) {
+        val title = context.getString(R.string.regex_replace)
+        val a = EditTextDialog(context)
+            .setTitle(title)
+            .setText(config.regexReplace)
+            .show {
+                config.regexReplace = it
+            }
+        a.setOnDismissListener {
+            finish()
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        finish()
+        return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        openRegexReplaceDialog(this)
+
+    }
+}
